390ac1d48caf078bdfb6efc758e4b74f3b5cfa93
VALux <olex3000@yandex.ru>
2016-01-31 17:58:58 +0200

    Ssh bot fixed. Need finalisation

--- build.sbt
+++ build.sbt
@@ -5,10 +5,9 @@ version := "1.0"
 scalaVersion := "2.11.7"
 
 resolvers += "scalac repo" at "https://raw.githubusercontent.com/ScalaConsultants/mvn-repo/master/"
+resolvers += "JAnalyse Repository" at "http://www.janalyse.fr/repository/"
 
 libraryDependencies ++= Seq (
   "io.scalac" %% "slack-scala-bot-core" % "0.2.1",
-  "com.decodified" %% "scala-ssh" % "0.7.0",
-  "org.bouncycastle" % "bcprov-jdk16" % "1.46",
-  "com.jcraft" % "jzlib" % "1.1.3"
+  "fr.janalyse"   %% "janalyse-ssh" % "0.9.19" % "compile"
 )
--- src/main/scala/bots/SshBot.scala
+++ src/main/scala/bots/SshBot.scala
@@ -1,30 +1,33 @@
 package bots
 
-import com.decodified.scalassh.{SSH, SshClient}
 import io.scalac.slack.MessageEventBus
 import io.scalac.slack.bots.AbstractBot
 import io.scalac.slack.common.{Command, OutboundMessage}
 
 /**
   * Created by alvo on 1/31/16.
-  *
+  * Bot for executing commands via ssh tunnel.
   */
 class SshBot(override val bus: MessageEventBus) extends AbstractBot {
 
-  private val trigger = "UATexec"
+  private val trigger = "SSH"
 
   override def help(channel: String): OutboundMessage = {
     OutboundMessage(channel, s"*$name* Will help you with quick performing some useful operation on UAT server \\n" +
     s"Usage: $trigger {operation} {arguments}")
   }
 
+  private def formatResponse(response: String): String = {
+    if(response.lines.nonEmpty) response.lines.mkString("\\n")
+    else response
+  }
+
   override def act: Receive = {
     case Command(`trigger`, operation :: args, message) =>
-      SSH("localhost") { client =>
-        client.exec("ls -l").right.map { result =>
-          publish(OutboundMessage(message.channel, "Result:\n" + result.stdOutAsString()))
-        }
+      val response = jassh.SSH.once("localhost", "alvo", "****", "****") { ssh =>
+        ssh.execute(s"""$operation ${args.mkString(" ")}""")
       }
+      publish(OutboundMessage(message.channel, s"```${formatResponse(response)}```"))
     case Command(`trigger`, _, message) =>
       publish(OutboundMessage(message.channel, "No arguments specified"))
   }
